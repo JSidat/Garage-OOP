62da416c2d037b8546439a5ee16d859fa645e92a
Junaid <JSidat95@gmail.com>
2021-02-18 17:43:26 +0000

    add garage class

--- src/garage/Garage.java
+++ src/garage/Garage.java
@@ -1,5 +1,25 @@
 package garage;
 
+import java.util.ArrayList;
+
 public class Garage {
+
+	private ArrayList<Vehicle> vehicles = new ArrayList<>();
+
+	public void addVehicle(Vehicle vehicle) {
+		vehicles.add(vehicle);
+	}
+
+	public void removeVehicle(Vehicle vehicle) {
+		vehicles.remove(vehicle);
+	}
+
+	public void calcBill(ArrayList<Vehicle> vehicles) {
+		for (Vehicle vehicle : vehicles) {
+			if (vehicle.getId() == 1)
+				;
+			System.out.println("Your bill is going to be £150");
+		}
+	}
 
 }
--- src/garage/GarageApp.java
+++ src/garage/GarageApp.java
@@ -3,7 +3,12 @@ package garage;
 public class GarageApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Vehicle car1 = new Car("Mercedes", "A-class", "black", 1, 4);
+
+		Vehicle motorbike1 = new Motorbike("Bmw", "S100RR", "Grey", 2, 3);
+
+		Vehicle bus1 = new Bus("Volvo", "longline", "Red", 3, 12);
 
 	}
 
